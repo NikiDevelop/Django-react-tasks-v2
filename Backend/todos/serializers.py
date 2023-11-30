9240c4f65c539731c04fac187db40c887dcefb0e
Nikifor Genchev <105102619+NikiDevelop@users.noreply.github.com>
2023-11-30 17:58:03 +0100

    Update serializers.py

--- Backend/todos/serializers.py
+++ Backend/todos/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from todos.models import Todo
 
-
+# Serializamos nuestro models para que luego pueda ser llamada en formato JSON y conectarse con el Front End mediante Axios
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
